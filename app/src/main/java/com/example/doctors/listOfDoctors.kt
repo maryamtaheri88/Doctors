453aa8337037a52cd6c3810d56de5832b7a55f91
MaryamTaheri <mary.irani@gmail.com>
2022-02-19 06:11:40 +0330

    complete list of doctor`s info

---
+++ app/src/main/java/com/example/doctors/listOfDoctors.kt
@@ -0,0 +1,11 @@
+package com.example.doctors
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class listOfDoctors : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_of_doctors)
+    }
+}
