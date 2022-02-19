41e6cad91dbc15114cadbf00482892f0cc56b957
MaryamTaheri <mary.irani@gmail.com>
2022-02-19 12:01:10 +0330

    complet intent and set and get data

--- app/src/main/java/com/example/doctors/MainActivity.kt
+++ app/src/main/java/com/example/doctors/MainActivity.kt
@@ -5,22 +5,16 @@ import android.os.Bundle
 import com.example.doctors.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
 
+    lateinit var binding :ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var name = intent.getStringExtra("name")
-        var address = intent.getStringExtra("address")
-        var phone = intent.getIntExtra("phone",0)
+        binding.textDoctorsName.text = intent.getStringExtra("name")
+        binding.textDoctorsAdresse.text =  intent.getStringExtra("address")
+        binding.textDoctorphone.text = intent.getIntExtra("phone",0).toString()
 
-        binding.textDoctorsName.text = name
-        binding.textDoctorsAdresse.text = address
-        binding.textDoctorphone.text = phone.toString()
     }
-
-
 }
--- app/src/main/java/com/example/doctors/listOfDoctors.kt
+++ app/src/main/java/com/example/doctors/listOfDoctors.kt
@@ -1,26 +1,37 @@
 package com.example.doctors
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.doctors.databinding.ActivityListOfDoctorsBinding
 import com.example.doctors.databinding.ActivityMainBinding
 
 class listOfDoctors : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding:ActivityListOfDoctorsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityListOfDoctorsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        listener()
     }
 
     private fun listener() {
-        binding
-        val pediatrician =Pediatrician()
 
+        var pediatrician = Pediatrician()
+
+        binding.aliAhmadi.setOnClickListener{
+
+
+             var aliAhmadi= Intent(this, MainActivity::class.java)
+
+            aliAhmadi.putExtra("name",pediatrician.listOfPediatrician[0].name)
+            aliAhmadi.putExtra("address",pediatrician.listOfPediatrician[0].address)
+            aliAhmadi.putExtra("phone",pediatrician.listOfPediatrician[0].phone)
+
+            startActivity(aliAhmadi)
+        }
     }
-
-
 }
-
